0e37340c6228d697e7cccc304a9d4181eacefc09
Lars Kotthoff <lars@larsko.org>
2011-09-17 20:21:46 +0100

    cables by decade

--- jsonify.rb
+++ jsonify.rb
@@ -3,25 +3,25 @@
 require 'json'
 
 visits70 = Hash.new
-f = File.open("visits-70.csv")
+f = File.open("sources-70-binned.csv")
 f.each { |l|
     v = l.chomp.split(/,/)
     visits70[v[1]] = v[0].to_i
 }
 visits80 = Hash.new
-f = File.open("visits-80.csv")
+f = File.open("sources-80-binned.csv")
 f.each { |l|
     v = l.chomp.split(/,/)
     visits80[v[1]] = v[0].to_i
 }
 visits90 = Hash.new
-f = File.open("visits-90.csv")
+f = File.open("sources-90-binned.csv")
 f.each { |l|
     v = l.chomp.split(/,/)
     visits90[v[1]] = v[0].to_i
 }
 visits00 = Hash.new
-f = File.open("visits-00.csv")
+f = File.open("sources-00-binned.csv")
 f.each { |l|
     v = l.chomp.split(/,/)
     visits00[v[1]] = v[0].to_i
@@ -30,7 +30,7 @@ f.each { |l|
 data = JSON.parse(STDIN.read)
 
 data["features"].each { |f|
-    f["p"].delete("v")
+    f["p"].delete("c")
     if visits70.has_key?(f["p"]["n"])
         f["p"]["70"] = visits70[f["p"]["n"]]
     else
