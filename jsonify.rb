200c9c99effd248015c388a6aab56db0814d4370
Lars Kotthoff <lars@larsko.org>
2011-09-17 20:18:38 +0100

    visits by decade

--- jsonify.rb
+++ jsonify.rb
@@ -2,18 +2,54 @@
 
 require 'json'
 
-visits = Hash.new
-f = File.open("visits.csv")
+visits70 = Hash.new
+f = File.open("visits-70.csv")
 f.each { |l|
     v = l.chomp.split(/,/)
-    visits[v[1]] = v[0].to_i
+    visits70[v[1]] = v[0].to_i
+}
+visits80 = Hash.new
+f = File.open("visits-80.csv")
+f.each { |l|
+    v = l.chomp.split(/,/)
+    visits80[v[1]] = v[0].to_i
+}
+visits90 = Hash.new
+f = File.open("visits-90.csv")
+f.each { |l|
+    v = l.chomp.split(/,/)
+    visits90[v[1]] = v[0].to_i
+}
+visits00 = Hash.new
+f = File.open("visits-00.csv")
+f.each { |l|
+    v = l.chomp.split(/,/)
+    visits00[v[1]] = v[0].to_i
 }
 
 data = JSON.parse(STDIN.read)
 
 data["features"].each { |f|
-    if visits.has_key?(f["p"]["n"])
-        f["p"]["v"] = visits[f["p"]["n"]]
+    f["p"].delete("v")
+    if visits70.has_key?(f["p"]["n"])
+        f["p"]["70"] = visits70[f["p"]["n"]]
+    else
+        f["p"]["70"] = 0
+    end
+    if visits80.has_key?(f["p"]["n"])
+        f["p"]["80"] = visits80[f["p"]["n"]]
+    else
+        f["p"]["80"] = 0
+    end
+    if visits90.has_key?(f["p"]["n"])
+        f["p"]["90"] = visits90[f["p"]["n"]]
+    else
+        f["p"]["90"] = 0
+    end
+    if visits00.has_key?(f["p"]["n"])
+        f["p"]["00"] = visits00[f["p"]["n"]]
+    else
+        f["p"]["00"] = 0
     end
 }
 
